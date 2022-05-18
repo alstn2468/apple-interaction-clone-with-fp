be3f140e6c1eded7339b41b0770c1890aeb2c737
Minsu Kim <alstn2468_@naver.com>
2022-05-18 17:46:08 +0900

    feat: Add mobile contents css style

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import './css/reset.css'
-import './css/style.css'
-import './css/global-nav.css'
+import './css/reset.css';
+import './css/style.css';
+import './css/footer.css';
+import './css/global-nav.css';
+import './css/scroll-section.css';
 
 console.log('Hello World');
